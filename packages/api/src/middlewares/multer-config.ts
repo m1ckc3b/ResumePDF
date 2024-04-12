6381ae37f26717ba52ed3e9c4c828849d7f92955
mickceb <cebulski.mi@gmail.com>
2024-04-12 22:32:11 +0200

    init multer

---
+++ packages/api/src/middlewares/multer-config.ts
@@ -0,0 +1,19 @@
+import multer from "multer"
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+  destination: (req: Express.Request, file: Express.Multer.File, callback: Function): void => {
+    callback(null, 'uploads')
+  },
+  filename: (req: Express.Request, file: Express.Multer.File, callback: Function): void => {
+    const name = file.originalname.split(' ').join('_')
+    callback(null, name)
+  }
+});
+
+export const upload = multer({storage: storage}).single('file')
