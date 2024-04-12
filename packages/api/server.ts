1751f792695ccfb504f5249787e2e0d4ba0cf4c3
mickceb <cebulski.mi@gmail.com>
2024-04-12 22:46:08 +0200

    updates

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,22 +1,22 @@
 import express from "express"
 import cors from "cors"
-import multer from "multer"
+import { upload } from "./src/middlewares/multer-config"
 
 const app = express()
 // app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
-const upload = multer({ dest: "uploads/"})
 
-app.get('/api', async (req:Request, res: Response) => {
+app.get('/api', async (req: Request, res: Response) => {
   res.send("Hello from ResumePDF ")
 })
 
-app.post("/api", upload.single("file"), (req: Request, res: Response) => {
+app.post("/api", upload, (req: Request, res: Response) => {
 
   const question = req.body.question
   const file = req.file
+  console.log(Bun.file(file.path));
 
   res.send("Data received")
   
