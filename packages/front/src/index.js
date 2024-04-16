23fd35b66139e184785a6dbdffd00196cf5b7afe
mickceb <cebulski.mi@gmail.com>
2024-04-16 13:05:53 +0200

    adding time execution tracking

--- packages/front/src/index.js
+++ packages/front/src/index.js
@@ -1,15 +1,24 @@
 const form = document.querySelector("#form")
+const fileInput = document.querySelector("#file")
 const fileInfo = document.querySelector('#file-info')
 const resume = document.querySelector('#file-resume')
+const reqExecution = document.querySelector("#req-execution")
 
 form.addEventListener("submit", submitForm)
+file.addEventListener("change", () => {
+  fileInfo.innerHTML = ''
+  resume.innerHTML = ''
+  reqExecution.innerHTML = ''
+})
 
 async function submitForm(e) {
   e.preventDefault()
 
   // const question = e.target.question.value
   const file = e.target.file.files[0]
-  console.log(file)
+  
+  // Time execution Start
+  const start = Date.now()
 
   const formData = new FormData()
   // formData.append("question", question)
@@ -19,6 +28,8 @@ async function submitForm(e) {
     method: "POST",
     body: formData,
   })
+  // Time execution End
+  const end = Date.now()
 
   form.reset()
 
@@ -30,5 +41,8 @@ async function submitForm(e) {
   resume.innerHTML = `
     <h2>Résumé</h2>
     <p>${await response.text()}</p>
+  `
+  reqExecution.innerHTML = `
+    <p>Réponse en : ${end - start} ms</p>
   `
 }
