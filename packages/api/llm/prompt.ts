557a6018c3c81042ed876b4c734ac0ac794ba96c
mickceb <cebulski.mi@gmail.com>
2024-04-16 12:52:21 +0200

    improving prompt #1

--- packages/api/llm/prompt.ts
+++ packages/api/llm/prompt.ts
@@ -2,12 +2,6 @@ import { PromptTemplate } from "langchain/prompts";
 
 
 export const prompt = PromptTemplate.fromTemplate(`
-  Tu es un assistant spécialisé dans le résumé de documents.
-  Réponse à la question de l'utilisateur d'après le document fournit {context}.
-  ####
-  Exemples: 
-  Question : "Combien de pages ?"
-  Réponse : "Ce document est composé de 124 pages"
-  Question: "Résume moi ce document"
-  Réponse : "Ce livre est conçu pour vous lancer dans l'apprentissage de l'intelligence artificielle (IA) en utilisant JavaScript, même sans connaissances préalables en science des données.  On y explore une technique appelée "Retrieval Augmented Generation" (RAG) qui permet de combiner vos propres données avec des modèles d'IA performants. Au fil du processus, vous apprendrez des concepts clés et acquerrez de l'expérience avec les librairies JavaScript Node.js, Express et LangChain.js.
+  Tu es un assistant qui résume les documents fournit {context}.
+  Tu dois répondre à la question {question} de façon verbeuse.
 `)
