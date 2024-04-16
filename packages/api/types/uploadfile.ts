ab854f56fb7782aaafa01ac3b6b6a726e4d711c6
mickceb <cebulski.mi@gmail.com>
2024-04-16 12:09:58 +0200

    init

---
+++ packages/api/types/uploadfile.ts
@@ -0,0 +1,10 @@
+export type uploadFilde = {
+  fieldname: string,
+  originalname: string,
+  encoding: string,
+  mimetype: string,
+  destination: string,
+  filename: string,
+  path: string,
+  size: number
+}
