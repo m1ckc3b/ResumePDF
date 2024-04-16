2dc69a096a0abc724303bf4060c1b90bb3020e0c
mickceb <cebulski.mi@gmail.com>
2024-04-16 11:44:32 +0200

    init server

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
-import { upload } from "./src/middlewares/multer-config"
+import { upload } from "./middlewares/multer-config"
+import main from "./llm";
 
 const app = express()
 // app.use(express.json())
@@ -8,18 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 
-app.get('/api', async (req: Request, res: Response) => {
+app.get('/api', async (req: Request, res: Response)=> {
   res.send("Hello from ResumePDF ")
 })
 
-app.post("/api", upload, (req: Request, res: Response) => {
+app.post("/api", upload, async (req: Request, res: Response) => {
+  const {question: string, file: File} = req.body
+  // Call LLM main function
+  const result = await main(question, file)
 
-  const question = req.body.question
-  const file = req.file
-  console.log(Bun.file(file.path));
-
-  res.send("Data received")
-  
+  res.send(result)
 })
 
 app.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`))
